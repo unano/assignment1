25b887bcba2ef042b9d10618227a56793def9739
GJQ <romanroll886@gmail.com>
2021-10-11 21:35:22 +0800

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./home";
-import Introduce from "./introduce";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
           <Switch>
-          <Route exact path="/~21057056g/introduce" component={Introduce} />
-            <Route exact path="/~21057056g" component={Home} />
-            <Redirect from="*" to="/~21057056g" />
+            <Route exact path="/" component={Home} />
+            <Redirect from="*" to="/" />
           </Switch>
     </BrowserRouter>
   );
